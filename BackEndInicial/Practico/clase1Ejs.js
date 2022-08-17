9a4374f5f109a9c97b189c2797431bb660028898
Martin c <mcabrera612@hotmail.com>
2022-08-17 01:16:26 -0300

    Arregle las carpetas del curso de HaDev

---
+++ BackEndInicial/Practico/clase1Ejs.js
@@ -0,0 +1,9 @@
+function removerVocales(str) {
+    let vocales = ["a", "e", "i", "o", "u"];
+    let resultado = [];
+    // usar replaceAll 
+    return resultado;
+}
+
+let str = "Hola como estas";
+console.log(removerVocales(str));

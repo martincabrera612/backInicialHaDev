f5baa71d25841c53a168fa884a3638f5fe9091de
Martin c <mcabrera612@hotmail.com>
2022-08-19 18:51:46 -0300

    Hay que corregir el ej3, falta 4 y 5

--- BackEndInicial/Practico/Clase2/ej3.js
+++ BackEndInicial/Practico/Clase2/ej3.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     let cont = 0;
+
     fs.appendFile("txt/ejemplo.txt", `Nueva Linea numero: ${cont} \n`, (err) => {
         cont++;
         if (err) {

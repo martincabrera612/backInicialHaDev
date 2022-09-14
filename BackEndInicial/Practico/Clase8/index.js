c89356ce54824917aced473170d74b670142d431
martincabrera <71331753+martincabrera612@users.noreply.github.com>
2022-09-14 15:12:09 -0300

    Clase8 archivos

---
+++ BackEndInicial/Practico/Clase8/index.js
@@ -0,0 +1,40 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+    "ha_ejercicio_mysql",
+    "root",
+    "root", {
+        host: "localhost", // 127.0.0.1
+        dialect: "mysql" //mysql
+    },
+
+);
+
+class User extends Model {}
+
+User.init({
+    fullname: DataTypes.STRING,
+    birthday: {
+
+        type: DataTypes.DATE,
+    },
+}, { sequelize: connection, modelName: user });
+
+async function main() {
+    await connection.sync({ force: true }) // OJO, EL FORCE TRUE CREA UNA TABLA SI NO EXISTE, Y SI YA EXISTE LA SOBREESCRIBE
+    console.log("Sincronizado...");
+
+    const usuariosDeLaBaseDeDatos = await User.findAll();
+
+    const nuevoUsuario = await User.create({
+        fullname: "Mariano Zunino",
+        birthday: new Date()
+    });
+
+    const user = await User.findOne();
+    console.log(user.fullname);
+    console.log(user.birthday);
+
+}
+
+main();
